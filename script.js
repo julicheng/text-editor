73c4e942c1a9adb048705bea564a2e52f7ae5e19
Juli Cheng <teachjuli@gmail.com>
2018-02-11 23:26:57 +0000

    create styles

---
+++ script.js
@@ -0,0 +1,25 @@
+let textInput = document.getElementById("textbox");
+let uppercaseButton = document.querySelector("#uppercase");
+let lowercaseButton = document.querySelector("#lowercase");
+let reverseButton = document.querySelector("#reverse");
+let colorButton = document.querySelector("#color");
+
+uppercaseButton.addEventListener("click", function() {
+	textInput.value = textInput.value.toUpperCase();
+})
+
+lowercaseButton.addEventListener("click", function() {
+	textInput.value = textInput.value.toLowerCase();
+})
+
+reverseButton.addEventListener("click", function() {
+	textInput.value = textInput.value.split("").reverse().join("");
+})
+
+colorButton.addEventListener("change", function() {
+	textInput.style.color = colorButton.value
+})
+
+
+
+
